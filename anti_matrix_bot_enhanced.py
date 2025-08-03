82cb64f28cba5b76f78d4e816199c4a97d4ab772
Sudhir Kumar <jakhalsudhir@gmail.com>
2025-08-03 17:02:41 +0530

    Update anti_matrix_bot_enhanced.py

--- anti_matrix_bot_enhanced.py
+++ anti_matrix_bot_enhanced.py
@@ -38,7 +38,7 @@ logger = logging.getLogger(__name__)
 
 #=== CONFIGURATION ===
 symbol = "dogeusdt"
-interval = "1m"
+interval = "5m"
 lookback = 150
 initial_equity = 100
 risk_per_trade = 0.1  # 1% risk per trade
